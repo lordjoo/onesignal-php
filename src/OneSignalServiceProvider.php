21cea8c7cee7996b5844b82e5d23aa8de05eeb13
youssef20000 <Yy%?2018%?>
2019-04-09 21:42:36 +0200

    fix config path

--- src/OneSignalServiceProvider.php
+++ src/OneSignalServiceProvider.php
@@ -24,7 +24,7 @@ class OneSignalServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'onesignal.php' => config_path('onesignal.php')
+            __DIR__.'/onesignal.php' => config_path('onesignal.php')
         ], 'config');
     }
 }
