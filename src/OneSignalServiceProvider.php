10b0c8ac4ae68e293bb6ed4a659c961ab9dc8b03
youssef20000 <Yy%?2018%?>
2019-04-09 16:37:08 +0200

    add structure files

---
+++ src/OneSignalServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Youssef\OneSignal;
+
+use Illuminate\Support\ServiceProvider;
+
+class OneSignalServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
